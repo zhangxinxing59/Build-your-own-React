06f8ed0fe818c2fdd9c4bbafb25c921cd477cc09
zhangxinxing1 <zhangxinxing1@xiaomi.com>
2021-04-20 17:52:33 +0800

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Step I: The createElement Function
+ * Step II: The render Function
+ * Step III: Concurrent Mode
+ * Step IV: Fibers
+ * Step V: Render and Commit Phases
+ * Step VI: Reconciliation
+ * Step VII: Function Components
+ * Step VIII: Hooks
+ */
+
