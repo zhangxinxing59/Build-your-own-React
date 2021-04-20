b711bd641b2ab017d42e73984dcfd8649422a3fa
zhangxinxing1 <zhangxinxing1@xiaomi.com>
2021-04-20 20:44:31 +0800

    add

--- index.js
+++ index.js
@@ -9,3 +9,33 @@
  * Step VIII: Hooks
  */
 
+// const element = <h1 title="foo">Hello</h1>
+
+// const element = {
+//     type: 'h1',
+//     props: {
+//         title: 'foo',
+//         children: 'Hello'
+//     }
+// }
+
+const element = React.createElement(
+    'h1',
+    { title: "foo"},
+    'Hello'
+)
+
+
+const container = document.getElementById("root")
+
+
+// ReactDOM.render(element, container)
+
+const node = document.createElement(element.type)
+node['title'] = element.props.title
+
+const text = document.createTextNode("")
+text['nodeValue'] = element.props.children
+
+
+
